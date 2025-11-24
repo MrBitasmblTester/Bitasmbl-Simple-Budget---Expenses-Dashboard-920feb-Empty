787406a097bf275e54844667ae83706c0ff863dc
MrBitasmblTester <21100302@ibsu.edu.ge>
2025-11-24 11:21:32 +0400

    Add +layout.svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  export let data;
+</script>
+
+<main class="app">
+  <header>
+    <h1>Simple Budget & Expenses Dashboard</h1>
+  </header>
+  <section class="content">
+    <slot />
+  </section>
+</main>
+
+<style>
+  .app { max-width: 960px; margin: 0 auto; padding: 1rem; }
+</style>
