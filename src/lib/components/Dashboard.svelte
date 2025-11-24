aa8dc3940e3571e33634dc6b817f4262481af503
MrBitasmblTester <21100302@ibsu.edu.ge>
2025-11-24 11:21:38 +0400

    Add Dashboard.svelte

---
+++ src/lib/components/Dashboard.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import EntryForm from './EntryForm.svelte';
+  import EntryList from './EntryList.svelte';
+  import SpendingChart from './SpendingChart.svelte';
+</script>
+
+<section>
+  <EntryForm />
+  <SpendingChart />
+  <EntryList />
+</section>
